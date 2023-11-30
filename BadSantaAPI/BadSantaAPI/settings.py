0b87fd2d2180939f5cce681b60c02fec5c44abac
David Mackenzie <dmackenzie1985@gmail.com>
2023-11-30 19:54:27 +1100

    removed cors check

--- BadSantaAPI/BadSantaAPI/settings.py
+++ BadSantaAPI/BadSantaAPI/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'BadSantaApp.apps.BadsantaappConfig',
     'rest_framework'
 ]
@@ -45,7 +44,6 @@ INSTALLED_APPS = [
 CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
