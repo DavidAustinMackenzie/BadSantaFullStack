ff22ad7c6b68ec58770989bad51435a758c4203c
David Mackenzie <dmackenzie1985@gmail.com>
2023-12-03 21:47:03 +1100

    added whitelist

--- BadSantaAPI/BadSantaAPI/settings.py
+++ BadSantaAPI/BadSantaAPI/settings.py
@@ -39,10 +39,13 @@ INSTALLED_APPS = [
     'rest_framework'
 ]
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = False
+CORS_ORIGIN_WHITELIST = [
+    'http://3.24.178.150/'
+]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
