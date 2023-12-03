f1e365ceaf40f9f84e36c5e5eda9cc9f6dcb1c7f
David Mackenzie <dmackenzie1985@gmail.com>
2023-12-03 14:30:10 +1100

    added / to routes

--- ui/BadSantaNG/src/app/app-routing.module.ts
+++ ui/BadSantaNG/src/app/app-routing.module.ts
@@ -8,12 +8,12 @@ import { AdminDrawComponent } from './admin-draw/admin-draw.component';
 import { AdminBadsantaComponent } from './admin-badsanta/admin-badsanta.component';
 
 const routes: Routes = [
-  {path:'',component:AddBadsantaComponent},
-  {path:'add-badsanta',component:AddBadsantaComponent},
-  {path:'badsanta',component:BadsantaComponent},
-  {path:'draw',component:DrawComponent},
-  {path:'admin-draw',component:AdminDrawComponent},
-  {path:'admin-badsanta',component:AdminBadsantaComponent}
+  {path:'/',component:AddBadsantaComponent},
+  {path:'/add-badsanta',component:AddBadsantaComponent},
+  {path:'/badsanta',component:BadsantaComponent},
+  {path:'/draw',component:DrawComponent},
+  {path:'/admin-draw',component:AdminDrawComponent},
+  {path:'/admin-badsanta',component:AdminBadsantaComponent}
 ];
 
 @NgModule({
