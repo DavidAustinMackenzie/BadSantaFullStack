8ceadbbe872318e1f359cadb2bbfcaf2e7708680
David Mackenzie <dmackenzie1985@gmail.com>
2023-12-04 16:49:21 +1100

    added missing comma to settings.py file

--- BadSantaAPI/BadSantaAPI/settings.py
+++ BadSantaAPI/BadSantaAPI/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders'
+    'corsheaders',
     'BadSantaApp.apps.BadsantaappConfig',
     'rest_framework'
 ]
